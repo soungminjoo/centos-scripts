f83cefbd5763747c4272d870dfa0ae028af93432
1004556 <soungmin@sk.com>
2020-07-27 21:14:32 +0900

    jenkins installation

--- installation/installJenkinsWithWar.sh
+++ installation/installJenkinsWithWar.sh
@@ -1,5 +1,7 @@
 mkdir jenkins
 cd jenkins
+
+sudo yum install wget
 wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
 
 echo "nohup java -jar jenkins.war --httpPort=9000 > jenkins.log 2>&1 &" > startJenkins.sh
