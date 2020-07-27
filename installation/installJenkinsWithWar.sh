8cffce04d0c4e77e99509b6d27b894015d8c0aec
1004556 <soungmin@sk.com>
2020-07-27 19:25:19 +0900

    jenkins installation

--- installation/installJenkinsWithWar.sh
+++ installation/installJenkinsWithWar.sh
@@ -5,5 +5,6 @@ wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
 echo "nohup java -jar jenkins.war --httpPort=9000 > jenkins.log 2>&1 &" > startJenkins.sh
 chmod 755 startJenkins.sh
 
-echo "Use ./startJenkins.sh"
-echo "Default port is 9000."
+echo "Latest 'jenkins.war' has downloaded and 'launch script' has created. (Default port : 9000)"
+echo "Please check ./jenkins directory"
+echo "And use 'startJenkins.sh' to start jenkins."
