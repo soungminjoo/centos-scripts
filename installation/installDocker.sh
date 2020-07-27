42efe5cb8dba34f4cf1b7395cf066fc2115fa887
1004556 <soungmin@sk.com>
2020-07-27 20:57:05 +0900

    jenkins installation

--- installation/installDocker.sh
+++ installation/installDocker.sh
@@ -1,5 +1,5 @@
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum -y install docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo chmod 666 /var/run/docker.sock
